3d69fbab5763803bba12cb240e006d251e5ac201
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-03-15 11:38:14 +0900

    init

---
+++ HelloWorld.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+             TemplateHaskell, OverloadedStrings #-}
+import Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler RepHtml
+getHomeR = defaultLayout [whamlet|Hello world!|]
+
+main :: IO ()
+main = warpDebug 3000 HelloWorld
